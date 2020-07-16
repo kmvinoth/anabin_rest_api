0f6ba68c731c049174f1eca1061b93eb6012a65c
vagrant <vagrant@vagrant.vm>
2020-07-16 19:26:18 +0000

    disable translate locale middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -125,7 +125,8 @@ AUTH_PASSWORD_VALIDATORS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
+    # Middleware for translation disabled to have everything in english 
+    # "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
