a7984c9e2bbc1fea1969e6404c499b046d6fa303
vagrant <vagrant@vagrant.vm>
2020-07-31 09:08:00 +0000

    fix mailgun invalid syntax env error

--- config/settings/production.py
+++ config/settings/production.py
@@ -128,10 +128,11 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-ANYMAIL = { 
-    MAILGUN_API_KEY = env("MAILGUN_API_KEY"), 
-    MAILGUN_API_URL = env("MAILGUN_API_URL"), 
-    MAILGUN_SENDER_DOMAIN = env("MAILGUN_SENDER_DOMAIN")}
+ANYMAIL = {
+    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
+    "MAILGUN_API_URL": env("MAILGUN_API_URL"),
+    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_SENDER_DOMAIN")
+    }
 # Collectfast
 # ------------------------------------------------------------------------------
 # https://github.com/antonagestam/collectfast#installation
