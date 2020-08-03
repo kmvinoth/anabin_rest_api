595e3dcce38f868d4a4b852e462c002a1ec78e6a
vagrant <vagrant@vagrant.vm>
2020-08-03 14:16:45 +0000

    add session auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -297,6 +297,7 @@ SOCIALACCOUNT_ADAPTER = "anabin_rest_api.users.adapters.SocialAccountAdapter"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
+        'rest_framework.authentication.SessionAuthentication'
     ),
     
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
