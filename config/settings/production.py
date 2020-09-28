a16c049e3b5ee3c9f2464ecb862ff40abcb0a8be
vagrant <vagrant@vagrant.vm>
2020-09-28 21:58:30 +0000

    add instance ip to allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["kmvinoth.org"])
+ALLOWED_HOSTS = ["18.192.120.255"]
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["kmvinoth.org"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
